d285f10daca919d258983a4115645b11cf96a7b7
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-07-15 03:55:25 +0300

    review

---
+++ javascript/code-challenges/challenges-14.test.js
@@ -0,0 +1,333 @@
+'use strict';
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 1 - Review
+
+Write a function named screenForNames that takes in an array of strings and uses Regex to create a new array of only those strings that match the following rules:
+
+* the name must begin with Mr., Mrs., Ms., Dr. followed by a space
+* the name must contain only letter characters (white spaces are ok)
+
+------------------------------------------------------------------------------------------------ */
+
+const screenForNames = (arr) => {
+  let result = []
+  let regPattern1 = /^Mr.(\w)*/g;
+  // let regPattern2 = /^Mrs.(\w)*/g;
+  let regPattern2 = /^Ms.(\w)*/g;
+  let regPattern3 = /^Dr.(\w)*/g;
+  let regPattern5 = /[a-zA-Z\s]*$/g;
+  arr.forEach(person => {
+    if (regPattern1.test(person) && regPattern5.test(person)) {
+      result.push(person)
+    }
+    if (regPattern2.test(person) && regPattern5.test(person)) {
+      result.push(person)
+    }
+    if (regPattern3.test(person)) {
+      result.push(person)
+    }
+  })
+  // console.log(result)
+  return result
+
+}
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 2
+
+Write a function named toTitleCase that takes in an array of strings and returns an array of strings with the first character in upper case and the rest as is.
+
+For example, ['apple', 'banana', 'MacGyver'] returns ['Apple', 'Banana', 'MacGyver'].
+------------------------------------------------------------------------------------------------ */
+
+const toTitleCase = (arr) => {
+  // Solution code here...
+  let regex = /(\b\w)/g
+  let result = arr.map(word => {
+    return word.replace(regex, word.substring(0, 1).toUpperCase())
+  })
+  // console.log(result)
+  return result
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 3
+
+Write a function named biggerThanLuke that, given the Star Wars data, below, returns the names of the characters whose mass is greater than Luke's.
+
+The names should be combined into a single string with each character name separated by a dash.
+
+For example, "Lando Calrisian - Boba Fett - Princess Amidala".
+------------------------------------------------------------------------------------------------ */
+
+let starWarsData = [{
+  name: 'Luke Skywalker',
+  height: '172',
+  mass: '77',
+  hair_color: 'blond',
+  skin_color: 'fair',
+  eye_color: 'blue',
+  birth_year: '19BBY',
+  gender: 'male',
+},
+{
+  name: 'C-3PO',
+  height: '167',
+  mass: '75',
+  hair_color: 'n/a',
+  skin_color: 'gold',
+  eye_color: 'yellow',
+  birth_year: '112BBY',
+  gender: 'n/a'
+},
+{
+  name: 'R2-D2',
+  height: '96',
+  mass: '32',
+  hair_color: 'n/a',
+  skin_color: 'white, blue',
+  eye_color: 'red',
+  birth_year: '33BBY',
+  gender: 'n/a'
+},
+{
+  name: 'Darth Vader',
+  height: '202',
+  mass: '136',
+  hair_color: 'none',
+  skin_color: 'white',
+  eye_color: 'yellow',
+  birth_year: '41.9BBY',
+  gender: 'male'
+},
+{
+  name: 'Leia Organa',
+  height: '150',
+  mass: '49',
+  hair_color: 'brown',
+  skin_color: 'light',
+  eye_color: 'brown',
+  birth_year: '19BBY',
+  gender: 'female'
+},
+{
+  name: 'Pex Kylar',
+  height: '180',
+  mass: '190',
+  hair_color: 'orange',
+  skin_color: 'brown',
+  eye_color: 'none',
+  birth_year: '27BBY',
+  gender: 'n/a'
+}];
+
+let biggerThanLuke = (arr) => {
+  // Solution code here...
+  // let regPattern1 = /^Luke(\w)*/g;
+  let max = 77
+  // console.log(max[mass])
+  let greaterLuke = []
+  arr.forEach(person => {
+    if (Number(person.mass) > max) {
+      greaterLuke.push(person.name)
+    }
+  })
+  // console.log(greaterLuke)
+  // console.log(greaterLuke.join(' - '))
+  return greaterLuke.join(' - ')
+
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 4
+Write a function named sortBy that takes in an array of objects, each of which has a particular property, and sorts those objects by that property, lowest to highest, returning the same array.
+
+Here is an example of the input:
+[
+  {name: 'Sweatshirt', price: 45},
+  {name: 'Bookmark', price: 2.50},
+  {name: 'Tote bag', price: 15}
+];
+
+This data could be sorted by name or price.
+------------------------------------------------------------------------------------------------ */
+
+const sortBy = (property, arr) => {
+  // Solution code here...
+  if (property == 'price') {
+    arr.sort((a, b) => {
+      if (a.price < b.price) {
+        return -1;
+      }
+      if (b.price > a.price) {
+        return 1;
+      }
+
+    });
+  }
+
+  if (property == 'name') {
+    arr.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (b.name > a.name) {
+        return 1;
+      }
+
+    });
+  }
+  return arr;
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 5 
+
+Write a function that determines if a given URL is secure, beginning with https://
+
+Guard against malformed URLs, such as: https:missing-slashes.bad
+
+For example:
+http://www.insecure.com returns false because the URL is not secure
+https://secure.com returns true because the URL is secure
+https:/missingslash.org returns false because the URL is malformed
+------------------------------------------------------------------------------------------------ */
+const isSecure = (url) => {
+  // Solution code here...
+  let flag = false
+  let scure = url.substring(0, 8)
+  // console.log(scure)
+  if (scure === 'https://') {
+    flag = true
+  }
+  return flag
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 6 
+
+Write a function named detectTicTacToeWin that accepts a two-dimensional array of strings. Each string is guaranteed to be either "X", "O" or an empty string. Your function should check to see if any row, column, or either diagonal direction has three matching "X" or "O" symbols (non-empty strings), three-in-a-line.
+
+This function should return either true or false to indicate if someone won the game.
+
+Instead of trying to write crazy for loops to automate checking the rows, columns and diagonals consider writing one helper function that accepts three coordinate pairs and checks the values of the array at those locations. For instance helpCheck(row1, col1, row2, col2, row3, col3).
+
+Your function does not need to work for boards of any size other than 3x3.
+
+Here is a sample board:
+[
+  ['X', '', 'O'],
+  ['X', 'O', ''],
+  ['X', 'O', 'X'],
+];
+------------------------------------------------------------------------------------------------ */
+
+const detectTicTacToeWin = (board) => {
+  // Solution code here...
+  let flag = false
+  for (let i = 0; i < board.length - 1; i++) {
+    
+    for (let j = 0; j < board.length - 1; j++) {
+      if (board[i][j] === 'X' || board[i][j] === 'O') {
+        if (board[i][j] === board[i + 1][j] || board[i][j] === board[i][j + 1] || board[i][j] === board[i + 1][j + 1]) {
+          flag = true
+        }
+        // if(board[i][j] !== board[i + 1][j] || board[i][j] !== board[i][j + 1] || board[i][j] !== board[i + 1][j + 1])
+        // {
+        //   flag =false
+        // }
+
+      
+    }
+  }
+}
+  return flag
+};
+
+/* ------------------------------------------------------------------------------------------------
+TESTS
+
+All the code below will verify that your functions are working to solve the challenges.
+
+DO NOT CHANGE any of the below code.
+
+Run your tests from the console: jest challenge-14.test.js
+
+------------------------------------------------------------------------------------------------ */
+
+describe('Testing challenge 1', () => {
+  test('It should return a list of names', () => {
+    const names = ['Mr. Brown', ' Ms. Red', 'Dr. Blue', 'Mrs.', '', 'Ms. Black', 'dr. Green', 'Mrs. Orange', 'Purple', 'Mr.  Pink'];
+    expect(screenForNames(names)).toStrictEqual(['Mr. Brown', 'Dr. Blue', 'Ms. Black', 'Mrs. Orange']);
+  });
+});
+
+describe('Testing challenge 2', () => {
+  test('It should convert each word to title case', () => {
+    const words = ['apple', 'banana', 'MacGyver'];
+    expect(toTitleCase(words)).toStrictEqual(['Apple', 'Banana', 'MacGyver']);
+
+    expect(toTitleCase([])).toStrictEqual([]);
+  });
+});
+
+describe('Testing challenge 3', () => {
+  test('It should return only characters that are bigger than Luke', () => {
+    expect(biggerThanLuke(starWarsData)).toStrictEqual('Darth Vader - Pex Kylar');
+    expect(biggerThanLuke([])).toStrictEqual('');
+  });
+});
+
+describe('Testing challenge 4', () => {
+  test('It should sort items by a price', () => {
+
+    expect(sortBy('price', [
+      { name: 'Sweatshirt', price: 45 },
+      { name: 'Bookmark', price: 2.50 },
+      { name: 'Tote bag', price: 15 }
+    ])).toStrictEqual([
+      { name: 'Bookmark', price: 2.50 },
+      { name: 'Tote bag', price: 15 },
+      { name: 'Sweatshirt', price: 45 },
+    ]);
+
+  });
+
+  test('It should sort items by name', () => {
+
+    expect(sortBy('name', [
+      { name: 'Sweatshirt', price: 45 },
+      { name: 'Bookmark', price: 2.50 },
+      { name: 'Tote bag', price: 15 }
+    ])).toStrictEqual([
+      { name: 'Bookmark', price: 2.50 },
+      { name: 'Sweatshirt', price: 45 },
+      { name: 'Tote bag', price: 15 },
+    ]);
+  });
+});
+
+describe('Testing challenge 5', () => {
+  test('It should check if url is https', () => {
+
+    expect(isSecure('http://www.insecure.com')).toBe(false);
+    expect(isSecure('https://secure.com')).toBe(true);
+    expect(isSecure('https:/missingslash.org')).toBe(false);
+  });
+});
+
+describe('Testing challenge 6', () => {
+  test('It should return true if there are three in a row', () => {
+    expect(detectTicTacToeWin([['X', '', 'O'], ['X', 'O', ''], ['X', 'O', 'X']])).toStrictEqual(true);
+    expect(detectTicTacToeWin([['O', '', 'X'], ['X', 'O', 'X'], ['X', '', 'O']])).toStrictEqual(true);
+  });
+
+  test('It should return false if there are not three in a row', () => {
+    expect(detectTicTacToeWin([['X', '', 'O'], ['O', 'O', ''], ['X', 'O', 'X']])).toStrictEqual(false);
+  });
+
+  test('It should not treat empty 3 in row as winner', () => {
+    expect(detectTicTacToeWin([['', '', ''], ['O', 'O', ''], ['X', 'O', 'X']])).toEqual(false);
+  });
+});
