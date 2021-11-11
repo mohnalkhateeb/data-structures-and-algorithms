7c148d5eccc0225ab3f1fa7b31e20d388b2f7ba5
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-11-12 01:08:03 +0200

    tree-inter

--- java/hashtable/lib/src/main/java/TreeIntersection/BinaryTree.java
+++ java/hashtable/lib/src/main/java/TreeIntersection/BinaryTree.java
@@ -5,7 +5,7 @@ import hashtable.HashNode;
 import java.util.ArrayList;
 
 public class BinaryTree<K,V> {
-  public ArrayList<HashNode<K,V>> preOrderList = new ArrayList<>();
+  public ArrayList<V> preOrderList = new ArrayList<>();
   public HashNode<K,V> root;
 
   public BinaryTree() {}
@@ -14,9 +14,9 @@ public class BinaryTree<K,V> {
     this.root = root;
   }
 
-  public ArrayList<HashNode<K,V>> preOrder(HashNode<K,V> root) {
+  public ArrayList<V> preOrder(HashNode<K,V> root) {
     try {
-      preOrderList.add((HashNode<K, V>) root.getValue());
+      preOrderList.add(root.getValue());
       if (root.leftChild != null) preOrder(root.leftChild);
       if (root.rightChild != null) preOrder(root.rightChild);
     } catch (NullPointerException exception) {
--- java/hashtable/lib/src/main/java/TreeIntersection/TreeIntersection.java
+++ java/hashtable/lib/src/main/java/TreeIntersection/TreeIntersection.java
@@ -1,13 +1,18 @@
 package TreeIntersection;
 
+import hashtable.HashNode;
+import hashtable.HashTable;
+
+import java.util.ArrayList;
+
 public class TreeIntersection {
 
   public static ArrayList treeIntersection(BinaryTree tree1, BinaryTree tree2) {
     if (tree1.root == null || tree2.root == null) return null;
-    HashTable<String> storeTable = new HashTable<>();
+    HashTable<String,Integer> storeTable = new HashTable<>();
     ArrayList<Object> arr = new ArrayList<>();
     for (Object oneItem : tree1.preOrder(tree1.root)) {
-      storeTable.add(oneItem.toString(), null);
+      storeTable.add(oneItem.toString(), (Integer) oneItem);
     }
     for (Object oneItem : tree2.preOrder(tree2.root)) {
       if (storeTable.contains(oneItem.toString()) && !arr.contains(oneItem)) arr.add(oneItem);
