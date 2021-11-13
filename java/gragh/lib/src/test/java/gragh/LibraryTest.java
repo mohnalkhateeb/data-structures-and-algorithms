150add30712a53125343239f8ee07ed4396f338d
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-11-13 17:16:44 +0200

    gragh

--- java/gragh/lib/src/test/java/gragh/LibraryTest.java
+++ java/gragh/lib/src/test/java/gragh/LibraryTest.java
@@ -8,7 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
-        Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+
     }
+  @Test
+  public void testGraph() {
+    Graph myGraph = new Graph();
+    assertNull(myGraph.toString());
+    myGraph.addNode("8");
+    assertEquals("[8]", myGraph.getNodes().toString());
+    myGraph.addEdge("8", "8");
+    assertEquals("[8]", myGraph.getNeighbors("8").toString());
+    assertEquals("{8=[8]}", myGraph.nodeList.toString());
+    myGraph.addNode("5");
+    myGraph.addEdge("8", "5");
+    assertEquals("[8, 5]", myGraph.getNeighbors("8").toString());
+    assertEquals("[8]", myGraph.getNeighbors("5").toString());
+    assertEquals("[5, 8]", myGraph.getNodes().toString());
+    assertEquals("{5=[8], 8=[8, 5]}", myGraph.nodeList.toString());
+    assertEquals(2, myGraph.size());
+
+  }
 }
